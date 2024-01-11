7558c0c1595d770224636f4775ca3c2da35a5de3
LAPTOP-OF-ALEX\Alex <carrasal@sheridancollege.ca>
2024-01-10 23:29:25 -0500

    Missile now moves in a straight line from where it is fired

---
+++ Assets/Week 1/Scripts/Missile.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    public float speed = 3f;
+    public GameObject missilePrefab;
+    public Transform spawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(10 * Time.deltaTime, 0, 0);
+    }
+}
