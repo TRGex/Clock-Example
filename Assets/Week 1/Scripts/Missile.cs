aca1a267efbe251555b0e51b95f5bb6c22e93987
LAPTOP-OF-ALEX\Alex <carrasal@sheridancollege.ca>
2024-01-17 10:26:00 -0500

    Task 1 Complete: Added moveable character that collides with wall and tank missiles move player

--- Assets/Week 1/Scripts/Missile.cs
+++ Assets/Week 1/Scripts/Missile.cs
@@ -8,15 +8,29 @@ public class Missile : MonoBehaviour
     public GameObject missilePrefab;
     public Transform spawn;
 
+    Rigidbody2D rigidbody;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(10 * Time.deltaTime, 0, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 direction = new Vector2(speed * Time.deltaTime, 0);
+        rigidbody.MovePosition(rigidbody.position + direction);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Missile collision");
+        Destroy(gameObject);
     }
 }
---
+++ Assets/Week 2/Scripts/Player.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    Vector2 direction;
+    public Rigidbody2D rigidbody;
+    public float force = 10f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction.x = Input.GetAxis("Horizontal");
+        direction.y = Input.GetAxis("Vertical");
+
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.AddForce(direction * force * Time.deltaTime);
+    }
+}
